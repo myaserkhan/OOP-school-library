adcfa531933bc4aee492e42f1c1df546d510f50d
Yasir Khan <khanyaser007@gmail.com>
2023-01-11 12:40:31 +0400

    Place optional parameters at the end

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person.rb'
 
 class Student < Person
-  def initialize(age, name: 'Unknown', parent_permission: true, classroom)
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
