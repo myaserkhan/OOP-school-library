02c0f2e31d9d0210a211713455f9559fd853f29f
Yasir Khan <khanyaser007@gmail.com>
2023-01-11 12:32:55 +0400

    Extend Person constructor with @classroom

--- student.rb
+++ student.rb
@@ -1,4 +1,8 @@
 require_relative './person.rb'
 
 class Student < Person
+  def initialize(age, name = 'Unknown', parent_permission = true, classroom)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
 end
