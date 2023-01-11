8c180e69a845fa604cb8a40baaff4d9519494eba
Yasir Khan <khanyaser007@gmail.com>
2023-01-11 12:42:56 +0400

    Add accessor for classrom and specialization

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,8 @@
 require_relative './person.rb'
 
 class Teacher < Person
+  attr_accessor :specialization
+
   def initialize(age, specialization, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @specialization = specialization
