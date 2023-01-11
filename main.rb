cd9df069c6f428095aaaf8f53cc124d8d553f6ce
Yasir Khan <khanyaser007@gmail.com>
2023-01-11 20:17:12 +0400

    Allow user to choose an option

--- main.rb
+++ main.rb
@@ -23,6 +23,27 @@ class Options
     user_choice = gets.chomp
     select_option(user_choice)
   end
+
+  def select_option(user_choice)
+    options = {
+      '1' => :list_all_books,
+      '2' => :list_all_people,
+      '3' => :create_person,
+      '4' => :create_book,
+      '5' => :create_rental,
+      '6' => :list_all_rentals,
+      '7' => :exit
+    }
+
+    method = options[user_choice]
+    if method.nil?
+      puts 'Invalid option, please try again!'
+      puts
+      show_menu
+    else
+      @app.send(method)
+    end
+  end
 end
 
 def main
