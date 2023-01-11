708572eed7a702e27b8a710c8362ec941eccb27b
Yasir Khan <khanyaser007@gmail.com>
2023-01-11 12:36:39 +0400

    Extend Person constructor with @specialization

--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,8 @@
 require_relative './person.rb'
 
 class Teacher < Person
+  def initialize(age, name = 'Unknown', parent_permission = true, specialization)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 end
