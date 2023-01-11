b025fe9420fea61c1d88aaab0fc09bd70e9331dc
Yasir Khan <khanyaser007@gmail.com>
2023-01-11 18:00:14 +0400

    Create has-many/belongs-to relation b/w Person and Rental

--- Entities/person.rb
+++ Entities/person.rb
@@ -1,7 +1,8 @@
 require_relative '../Decorator/nameable'
+require_relative '../Relationship/rental'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
@@ -10,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
@@ -26,5 +28,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
   end
 end
--- Relationship/rental.rb
+++ Relationship/rental.rb
@@ -1,10 +1,12 @@
 class Rental
-  attr_accessor :date, :book
+  attr_accessor :date, :book, :person
 
-  def initialize(date, book)
+  def initialize(date, book, person)
     @date = date
     @book = book
+    @person = person
 
     book.rentals << self
+    person.rentals << self
   end
 end
