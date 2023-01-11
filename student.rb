849f976f5295024a70c8343bc8d363ef963d2b43
Yasir Khan <khanyaser007@gmail.com>
2023-01-11 12:39:22 +0400

    Use keyword arguments for constructor

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person.rb'
 
 class Student < Person
-  def initialize(age, name = 'Unknown', parent_permission = true, classroom)
+  def initialize(age, name: 'Unknown', parent_permission: true, classroom)
     super(age, name, parent_permission)
     @classroom = classroom
   end
