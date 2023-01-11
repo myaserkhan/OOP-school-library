41b483c0c3081e097b0fc51752578fac0a8955fe
Yasir Khan <khanyaser007@gmail.com>
2023-01-11 12:41:41 +0400

    Update super with keyword arguments

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative './person.rb'
 
 class Teacher < Person
   def initialize(age, specialization, name: 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
