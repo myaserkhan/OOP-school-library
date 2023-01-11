766e2ed08df83547932c642011438bf5598b03be
Yasir Khan <khanyaser007@gmail.com>
2023-01-11 12:25:15 +0400

    Add a public method

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -13,5 +13,11 @@ class Person
 
   def is_of_age?
     @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    is_of_age? || @parent_permission
   end
 end
