9020382b986fc21266d9118d28ed127678143ec9
Yasir Khan <khanyaser007@gmail.com>
2023-01-18 20:55:51 +0400

    Updat Storage class

--- Modules/storage.rb
+++ Modules/storage.rb
@@ -3,8 +3,8 @@ class Storage
   @base_url = './db/'
 
   def self.save_data(class_name, object)
-    # Create the directory if it doesn't exist
-    Dir.mkdir(@base_url) unless Dir.exist?(@base_url)
+    # Create the directory 
+    Dir.mkdir(@base_url) 
 
     file_path = "#{@base_url}#{class_name}.#{@file_extension}"
     File.write(file_path, JSON.generate(object))
