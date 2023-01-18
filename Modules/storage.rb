1d7ce6ecde54076ece9481a31f85d12db6267fcf
Yasir Khan <khanyaser007@gmail.com>
2023-01-18 21:24:54 +0400

    Update storage file

--- Modules/storage.rb
+++ Modules/storage.rb
@@ -1,17 +1,13 @@
 class Storage
-  @file_extension = 'json'
+  @file_extention = 'json'
   @base_url = './db/'
 
   def self.save_date(class_name, object)
-    # Create the directory
-    Dir.mkdir(@base_url)
-
-    file_path = "#{@base_url}#{class_name}.#{@file_extension}"
-    File.write(file_path, JSON.generate(object))
+    File.write("#{@base_url}#{class_name}.#{@file_extention}", JSON.generate(object))
   end
 
   def self.load_data(class_name)
-    file_path = "#{@base_url}#{class_name}.#{@file_extension}"
+    file_path = "#{@base_url}#{class_name}.#{@file_extention}"
 
     return [] unless File.exist?(file_path)
 
