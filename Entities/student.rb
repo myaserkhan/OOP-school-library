356c4740af589a1080f3e3c2451fb3f6bd4d6551
dadadon <davianberoni@yahoo.com>
2023-01-18 09:45:38 -0500

    Create a JSON addition for teacher.rb

--- Entities/student.rb
+++ Entities/student.rb
@@ -5,6 +5,7 @@ class Student < Person
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
   end
 
   def play_hooky
