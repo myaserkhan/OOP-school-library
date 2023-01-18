8f13b50e2b414264126b3e7765c9be961e823164
dadadon <davianberoni@yahoo.com>
2023-01-18 09:48:16 -0500

    Fix typo in book.rb

--- Relationship/book.rb
+++ Relationship/book.rb
@@ -19,7 +19,7 @@ class Book
   def to_json(*args)
     {
       JSON.create_id => self.class.name,
-      'title' => @titile,
+      'title' => @title,
       'author' => @author
     }.to_json(*args)
   end
